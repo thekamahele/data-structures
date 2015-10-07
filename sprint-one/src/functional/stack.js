7bdd119a82c38d8e797bf9f6cde508d224f8aaaa
thekamahele <jira.vinyoo@gmail.com>
2015-10-07 14:24:24 -0700

    Completed code for functions in stack.js to pass functional pattern tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,26 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var length = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    length++;
+    storage[length] = value;
   };
 
   someInstance.pop = function() {
+    var result = storage[length];
+
+    if(length > 0) {
+      delete storage[length];
+      length--;
+    }
+
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
