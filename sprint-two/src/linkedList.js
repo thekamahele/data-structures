1d907286ab620a87f70e3e11724263febd7aa2be
thekamahele <jira.vinyoo@gmail.com>
2015-10-08 10:35:37 -0700

    Passed tests for linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create a new node
+    var newNode = Node(value);
+    
+    var temp = list.head;
+    //debugger;
+    if(list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+   
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    //result variable
+    var result = list.head;
+    //assign new head value
+    list.head = list.head.next;
+    //return the former head
+    return result.value;
   };
 
   list.contains = function(target) {
+    var temp = list.head;
+    while(temp) {
+      if(temp.value===target){
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
+      //temp.next = newNode;
+
   };
 
   return list;
