d9ca4d33945b64190588e7684ef63b262194f6ed
thekamahele <jira.vinyoo@gmail.com>
2015-10-08 11:11:15 -0700

    Passed tests for addChild and contains for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+ 
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -12,10 +14,22 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  this.children[this.children.length] = Tree(value);  // fix me
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, childrenArray) {
+  var childs = childrenArray || this.children;
+
+  //iterate through children of tree
+  for(var i = 0; i < childs.length; i++){
+    if(childs[i].value===target){
+      return true;
+    }
+    if(childs[i].children.length!==0){
+      return this.contains(target, childs[i].children);
+    }
+  }
+ return false;
 };
 
 
