074436a915abd4e90184949dc8020ab3fd70af65
thekamahele <jira.vinyoo@gmail.com>
2015-10-09 11:14:16 -0700

    Passed all specs for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -34,14 +34,19 @@ Graph.prototype.removeNode = function(node) {
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var list = this.adjacencyList[fromNode];
 
-  for(var i = 0; i < list.length)
+  for(var i = 0; i < list.length; i++) {
+    if(list[i] === toNode) {
+      return true;
+    }
+  } 
+  return false;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   this.edgeList.push([fromNode, toNode]);
-  this.edgeList.push([toNode, fromNode]);
+  //this.edgeList.push([toNode, fromNode]);
 
   this.adjacencyList[fromNode].push(toNode);
   this.adjacencyList[toNode].push(fromNode);
@@ -51,11 +56,27 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var list = this.edgeList;
+  for(var i =0; i<list.length; i++){
+    if(_.contains(list[i], fromNode) && _.contains(list[i], toNode)){
+      delete list[i];
+    }
+  }
+  //remove toNode from fromNode's adjacency list
+  this.adjacencyList[fromNode] = _.without(this.adjacencyList[fromNode], toNode);
+
+  //remove fromNode from toNode's adjacency list
+  this.adjacencyList[toNode] = _.without(this.adjacencyList[toNode], fromNode);
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+
+  _.each(this.adjacencyList, function(element, node) {
+    cb(node);
+  });
+
 };
 
 /*
