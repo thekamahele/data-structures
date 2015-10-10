542ceceb3cc5e1b6e5e515752af8be37d0780e8d
thekamahele <jira.vinyoo@gmail.com>
2015-10-10 10:04:09 -0700

    Set up binary search tree, passed should have methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,28 @@
 var BinarySearchTree = function(value) {
+  var tree = {};
+  tree.value = value;
+  tree.left;
+  tree.right;
+
+  _.extend(tree, binaryMethods);
+
+  return tree;
+
 };
 
+var binaryMethods = {};
+
+binaryMethods.insert = function(value) {
+
+};
+
+binaryMethods.contains = function(value) {
+
+};
+
+binaryMethods.depthFirstLog = function(cb) {
+//each
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
