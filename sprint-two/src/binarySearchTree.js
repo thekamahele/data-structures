d06ec0cf48b71fccc45930605a654d2d2df5e05d
thekamahele <jira.vinyoo@gmail.com>
2015-10-10 10:53:19 -0700

    Implemented binary search tree to pass all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,15 +13,55 @@ var BinarySearchTree = function(value) {
 var binaryMethods = {};
 
 binaryMethods.insert = function(value) {
+  
+  if(value < this.value) {
+    if(this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if(this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
 
 };
 
 binaryMethods.contains = function(value) {
+  if(value === this.value) {
+    return true;
+  }
 
+  if(value < this.value) {
+    if(this.left === undefined) {
+      return false;
+    } else {
+     return this.left.contains(value);
+    }
+  } else {
+    if(this.right === undefined) {
+      return false;
+    } else {
+     return this.right.contains(value);
+    }
+  }
+  return false;
 };
 
-binaryMethods.depthFirstLog = function(cb) {
-//each
+binaryMethods.depthFirstLog = function(cb) {  
+  // calls cb on value of node
+  cb(this.value);
+    // make recursive call with node.left
+  if(this.left !== undefined){
+    this.left.depthFirstLog(cb);
+  }
+  // make recursive call with node.right
+  if(this.right !== undefined){
+    this.right.depthFirstLog(cb);
+  }
 };
 
 /*
